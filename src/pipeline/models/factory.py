58093d18fe67dd78e7ee370ed29c5b265555fba6
apernea <apernea31@gmail.com>
2026-03-09 17:27:24 +0200

    Implement ModelFactory for model creation and loading: Introduced a static method to create and load models based on their type, and removed the obsolete ModelInterface class from model.py.

--- src/pipeline/models/factory.py
+++ src/pipeline/models/factory.py
@@ -1,6 +1,25 @@
-from dataclasses import dataclass
+from pipeline.interfaces.model import ModelInterface
+from pipeline.utils.registry import Registry
+
+model_registry = Registry[ModelInterface]()
 
 
-@dataclass
+class ModelType:
+    LSTM = "lstm"
+    LINEAR_REGRESSION = "linear_reg"
+    RANDOM_FOREST = "random_forest"
+
+
 class ModelFactory:
-    raise NotImplementedError
+    @staticmethod
+    def create(model_type: str) -> ModelInterface:
+        cls = model_registry.get(model_type)
+        return cls()
+
+    @staticmethod
+    def load(model_type: str, directory: str, prefix: str) -> ModelInterface:
+        """Loads a previously saved model by type name."""
+        cls = model_registry.get(model_type)
+        instance = cls()
+        instance.load(directory, prefix)
+        return instance
