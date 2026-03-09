4cbbd507801b95e8aa401c99f7662d4e19b636f8
apernea <apernea31@gmail.com>
2026-03-09 16:39:37 +0200

    """ Replace fetcher.py with new PostgreSQL and database modules: Introduced postgre.py for PostgreSQL data access and database.py as a base provider for PostgreSQL connections. """

---
+++ src/pipeline/provider/common/database.py
@@ -0,0 +1,3 @@
+"""Base DB provider for PostgreSQL connections
+
+"""
---
+++ src/pipeline/provider/postgre.py
@@ -0,0 +1,3 @@
+"""
+PostgreSQL-based data provider for accesing DB linked to this project, made for retrieving and storing data into it. 
+"""
